ddcf04548ec3e5aa25441b1d3acee2e94fdc1ae7
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-10-09 12:41:34 +0200

    build: build out the website

--- vite.config.js
+++ vite.config.js
@@ -3,31 +3,35 @@ import { resolve } from "path";
 import dts from "vite-plugin-dts";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const isLibMode = process.env.BUILD_MODE === "lib";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
   ],
-  build: {
-    copyPublicDir: false,
-    lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
-      name: "MyElement",
-      fileName: "my-element",
-    },
-    rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime"],
-      output: {
-        globals: {
-          preserveModules: true,
-          react: "React",
-          "react-dom": "ReactDOM",
-          "react/jsx-runtime": "react/jsx-runtime",
+  build: isLibMode
+    ? {
+        copyPublicDir: false,
+        lib: {
+          entry: resolve(__dirname, "lib/main.ts"),
+          name: "MyElement",
+          fileName: "my-element",
         },
-      },
-    },
-  },
+        rollupOptions: {
+          external: ["react", "react-dom", "react/jsx-runtime"],
+          output: {
+            globals: {
+              preserveModules: true,
+              react: "React",
+              "react-dom": "ReactDOM",
+              "react/jsx-runtime": "react/jsx-runtime",
+            },
+          },
+        },
+      }
+    : {},
   server: {
-    open: "/src/index.html",
+    open: "/index.html",
   },
 });
